390ec98590a2661cda98c86accf73a8e203178fa
Adolfo10 <adolfo.hinojosa99@gmail.com>
2019-02-21 00:55:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/adolfohinojosa/practica6filter/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.adolfohinojosa.practica6filter;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String t = getIntent().getStringExtra("Mensaje");
+        textv = findViewById(R.id.txtTexto);
+        if (t != null){
+            textv.setText(t);
+        }
+    }
+}
